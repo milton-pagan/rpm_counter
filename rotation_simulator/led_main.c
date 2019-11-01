2e8fba12267ba955435b5dcc5eea9df7734ab9f5
Milton Pagan <43056463+milton-pagan@users.noreply.github.com>
2019-11-01 16:30:46 -0400

    Initial commit

---
+++ rotation_simulator/led_main.c
@@ -0,0 +1,64 @@
+#include <msp430.h> 
+
+#define LED BIT0
+#define BUTTON BIT3
+#define MAX_COUNT 0x1800
+#define SHIFTS 3
+
+/**
+ * main.c
+ */
+
+int shifts;
+int count;
+
+int main(void)
+{
+    shifts = SHIFTS;
+    count = MAX_COUNT;
+
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	PM5CTL0 &= ~LOCKLPM5;
+
+	P1DIR |= LED;
+	P1OUT &= ~LED;
+
+	P2DIR &= ~BUTTON;
+	P2REN |= BUTTON;
+	P2OUT |= BUTTON;
+	P2IES |= BUTTON;
+	P2IE  |= BUTTON;
+
+	TA0CTL |= TASSEL__ACLK | ID__8 | MC__UP;
+	TA0CCTL0 |= CCIE;
+
+	TA0CCR0 = count;
+
+	_BIS_SR(CPUOFF + GIE);
+
+	while(1) {}
+
+}
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A(void) {
+    P1OUT ^= LED;
+    TA0CCR0 = count;
+}
+
+#pragma vector=PORT2_VECTOR
+__interrupt void PORT_2(void) {
+    if(shifts == 0) {
+        shifts = SHIFTS;
+        count = MAX_COUNT;
+    }
+
+    else {
+        count--;
+        count >>= 1;
+    }
+
+    shifts--;
+    P2IFG &= ~BUTTON;
+}
