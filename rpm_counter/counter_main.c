fc3b3acbb6247b65c0bd2eaaf279b444b2e955c0
dionel <dionel@MSI>
2019-11-03 16:33:20 -0400

    Refactor ISR P2

--- rpm_counter/counter_main.c
+++ rpm_counter/counter_main.c
@@ -2,14 +2,21 @@
 
 #define LED BIT7
 #define BUTTON1 BIT3 //start
-#define BUTTON2 BIT7 //stop || led unit change
+#define BUTTON2 BIT7 //stop || LED unit change
 #define FREQ 4096
+#define TA1CMP 1024 // Control LED flash velocity
 
-long signal_count, time, timer_count;
+// Prototypes
+void clear_global_variables();
 
+// Global Variables
+static long signal_count, time, timer_count, result;
 int measuring = 0;
-int lastDigit = 0;
 
+int result_digits[3];
+unsigned int index, current_digit;
+
+// Functions
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
@@ -50,7 +57,7 @@ int main(void)
 // Timer A0 ISR
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer0_A_CC0_ISR(){
-    timerCount += 0xFFFF;
+    timer_count += 0xFFFF;
 }
 
 // Timer A1 ISR
@@ -75,24 +82,60 @@ __interrupt void Buttons_ISR() {
     // BUTTON1 pressed
     if(P2IFG == BUTTON1) {
         P2IFG &= ~BUTTON1;
+
         if(measuring == 1){
             return;
         }
+        TA1CCR0 = 0;
+        P1OUT &= ~LED;
         measuring = 1;
         P1IE |= BIT0;
-        signal_count = 0;
-
+        clear_global_variables();
+        TA0CCR0 = 1;
         return;
     }
     // BUTTON2 pressed
     else {
         P2IFG &= ~BUTTON2;
-
         if(measuring == 1){
+            TA0CCR0 = 0;
             measuring = 0;
+            P1IE &= ~BIT0;
+
+            // Calculations
+            timer_count += TA0R;
+            time = timer_count/FREQ;
+            // time to minutes
+            time /= 60;
+
+            result = signal_count / time;
+
+            result_digits[2] = result % 10;
+            result_digits[1] = (result / 10) % 10;
+            result_digits[0] = (result / 100) % 10;
+            current_digit = result_digits[index++];
+
+            TA1CCR0 = TA1CMP;
+
             return;
         }
 
+        // LED switch units
+        else{
+            if(index == 3) index = 0;
+            current_digit = result_digits[index++];
+
+        }
 
     }
+}
+
+void clear_global_variables(){
+    signal_count = 0;
+    time = 0;
+    timer_count = 0;
+    result = 0;
+    index = 0;
+    current_digit = 0;
+
 }
