6eef99a9d4b5686baa3a46f4f213117a327ffc87
Milton Pagan <43056463+milton-pagan@users.noreply.github.com>
2019-11-04 11:55:51 -0400

    Refactor PORT1 ISR

--- rpm_counter/counter_main.c
+++ rpm_counter/counter_main.c
@@ -4,17 +4,17 @@
 #define BUTTON1 BIT3 //start
 #define BUTTON2 BIT7 //stop || LED unit change
 #define FREQ 4096
-#define TA1CMP 2048 // Control LED flash velocity
+#define ONE_SEC 2048 // Control LED flash velocity
 
 // Prototypes
 void clear_global_variables();
 
 // Global Variables
-static long signal_count, time, timer_count;
+static long signal_count, timer_digit;
 int measuring = 0;
 
 int result_digits[3];
-unsigned int index, current_digit, timer_digit, timer_delay, delay_on, result;
+unsigned int index, current_digit, result, time;
 
 // Functions
 void main()
@@ -29,7 +29,7 @@ void main()
     P1DIR &=  ~BIT0;
     P1IN &= ~BIT0;
     P1IE &= ~BIT0;
-    P1IES |= BIT0;
+    P1IES &= ~BIT0;
 
     // Initialize 1.7 as output
     P1DIR |= LED;
@@ -45,7 +45,7 @@ void main()
     // Initialize timer A0
     TA0CCR0 = 0;
     TA0CCTL0 |= CCIE;
-    TA0CTL |= TASSEL__ACLK + ID_3 + MC__CONTINUOUS;
+    TA0CTL |= TASSEL__ACLK + ID_3 + MC__UP;
 
     // Initialize timer A1
     TA1CCR0 = 0;
@@ -60,36 +60,28 @@ void main()
 // Timer A0 ISR
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer0_A_CC0_ISR(){
-    timer_count += 0xFFFF;
+    time += 1;
 }
 
 // Timer A1 ISR
 #pragma vector=TIMER1_A0_VECTOR
 __interrupt void Timer1_A_CC0_ISR() {
-    if(!delay_on && timer_digit == 0) {
+    if(TA1CCR0 == 10 * ONE_SEC) {
         timer_digit = 2 * current_digit;
-        P1OUT &= ~LED;
+        TA1CCR0 = ONE_SEC;
     }
 
-    if(delay_on && timer_delay == 0) {
-        P1OUT |= LED;
-        timer_delay = 6; // Changed from 20 -Dionel
-    }
-
-    if(timer_delay == 0) {
+    if(timer_digit) {
         P1OUT ^= LED;
 
-        if(timer_digit - 1 == 0) delay_on = 1;
         timer_digit--;
+        TA1CCR0 = ONE_SEC;
     }
 
-    if(delay_on && timer_delay != 0) {
-
-        if(timer_delay - 1 == 0) delay_on = 0;
-        timer_delay--;
+    if(!timer_digit) {
+        P1OUT |= LED;
+        TA1CCR0 = 10 * ONE_SEC;
     }
-
-    TA1CCR0 = TA1CMP;
 }
 
 //Input ISR
@@ -102,8 +94,6 @@ __interrupt void input_ISR() {
 // Buttons ISR
 #pragma vector=PORT2_VECTOR
 __interrupt void Buttons_ISR() {
-
-    long temp = TA0R;
 
     // Both buttons pressed
     if(P2IFG == (BUTTON1 | BUTTON2)) {
@@ -122,7 +112,7 @@ __interrupt void Buttons_ISR() {
         measuring = 1;
         clear_global_variables();
         P1IE |= BIT0;
-        TA0CCR0 = 1;
+        TA0CCR0 = FREQ;
         return;
     }
     // BUTTON2 pressed
@@ -134,21 +124,21 @@ __interrupt void Buttons_ISR() {
             measuring = 0;
 
             // Calculations
-            timer_count += temp;
-            time = timer_count/FREQ;
-            double aux = signal_count / time;
-            aux *= 60;
 
-            result = (int) aux;
-            //            result = 123;
+            result = signal_count / time;
+            result *= 60;
 
             result_digits[2] = result % 10;
             result_digits[1] = (result / 10) % 10;
             result_digits[0] = (result / 100) % 10;
 
-            current_digit = result_digits[0];
+            current_digit = result_digits[index++];
 
-            TA1CCR0 = TA1CMP;
+            TA1CCR0 = ONE_SEC;
+
+            P1OUT &= ~LED;
+
+            timer_digit = 2 * current_digit;
 
             return;
         }
@@ -157,6 +147,9 @@ __interrupt void Buttons_ISR() {
         else{
             if(index == 3) index = 0;
             current_digit = result_digits[index++];
+            P1OUT &= ~LED;
+            timer_digit = 2 * current_digit;
+            TA1CCR0 = ONE_SEC;
         }
 
     }
@@ -165,11 +158,9 @@ __interrupt void Buttons_ISR() {
 void clear_global_variables(){
     signal_count = 0;
     time = 0;
-    timer_count = 0;
     result = 0;
     index = 0;
     current_digit = 0;
     timer_digit = 0;
-    delay_on = 0;
 
 }
